903e6e29eeb77fef726adf54bf3a8d36f7076666
Achim Königs <garfonso@mobo.info>
2014-12-16 19:54:39 +0100

    activity complete redesigned.

--- service/javascript/assistants/AssignMessages.js
+++ service/javascript/assistants/AssignMessages.js
@@ -39,6 +39,8 @@ function setRev (newRev) {
 	messageQuery.where[0].val = newRev;
 }
 
+var newRev = 0;
+
 var AssignMessages = function () { "use strict"; };
 
 AssignMessages.prototype.processMessage = function (msg) {
@@ -160,15 +162,13 @@ AssignMessages.prototype.processMessageAndAddress = function (msg, address) {
 
 	future.then(function msgMergeCB() {
 		var result = checkResult(future);
-		Log.debug("Message stored: ", result);
-
+		console.log("Message stored: ", result);
 		if (result.returnValue) {
-			//update rev to store msg again, if assigned to multiple conversations, i.e. multiple recepients.
-			msg._rev = result.results[0].rev;
-			future.result = {returnValue: true};
-		} else {
-			future.result = result; //propagate errors.
+			if (result.rev > newRev) {
+				newRev = result.rev;
+			}
 		}
+		future.result = {returnValue: true};
 	});
 
 	return future;
@@ -178,8 +178,7 @@ AssignMessages.prototype.run = function (outerFuture) {
 	"use strict";
 	var args = this.controller.args,
 		future = new Future(),
-		rev = args.lastCheckedRev || 0,
-		newRev = 0;
+		rev = args.lastCheckedRev || 0;
 
 	setRev(rev);
 	future.nest(DB.find(messageQuery, false, false));
@@ -207,20 +206,25 @@ AssignMessages.prototype.run = function (outerFuture) {
 		}
 	});
 
-	future.then(this, function updateRev() {
-		var result = future.result; //read result.
-		setRev(newRev);
-		future.nest(PalmCall.call("palm://com.palm.activitymanager/", "create", {
-			activity: activity,
-			start: true,
-			replace: true
-		}));
-	});
-
 	future.then(this, function processingFinished() {
 		var result = future.result;
-		Log.debug("Activity restored: " + JSON.stringify(result));
-		outerFuture.result = {};
+		outerFuture.result = result;
 	});
 	return outerFuture;
+};
+
+AssignMessages.prototype.complete = function (activityObject) {
+	var restart = false;
+	Log.debug("Completing ", activityObject);
+	if (activityObject.name === activity.name) {
+		Log.debug("Need to restart.");
+		setRev(newRev);
+		activityObject.setTrigger("fired",
+								  "palm://com.palm.db/watch",
+								  { query: messageQuery });
+		activityObject.setCallback("palm://org.webosports.service.messaging/assignMessages",
+								   { lastCheckedRev: 0 });
+		restart = true;
+	}
+	activityObject.complete(restart);
 };
